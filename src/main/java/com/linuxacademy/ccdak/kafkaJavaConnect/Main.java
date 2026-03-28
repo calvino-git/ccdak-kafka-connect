bdba379b9f8c8279672d2898fe89e90bcacb9278
calviniloki <bbbebe661@yahoo.fr>
2026-03-28 16:17:13 +0000

    Update the kafka config

--- src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
@@ -1,5 +1,6 @@
 package com.linuxacademy.ccdak.kafkaJavaConnect;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -19,15 +20,16 @@ public class Main {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
         props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 5000);
         props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
 
         try(Producer<String, String> producer = new KafkaProducer<>(props)){
             for(int i = 0; i < 1000; i+=5){
-                System.out.println("Index : " + i);
-		Thread.sleep(5000);
-                Future<RecordMetadata> future = producer.send(new ProducerRecord<>("streams-input-topic", Integer.toString(i), "Count " + i));
+                String randomString = RandomStringUtils.randomAlphabetic(1);
+                System.out.println(i + "->" + randomString);
+                Future<RecordMetadata> future = producer.send(new ProducerRecord<>("streams-input-topic", Integer.toString(i), randomString));
                 RecordMetadata metadata = future.get();
                 System.out.println(metadata);
-                
+                Thread.sleep(1000);
             }
         }catch (Exception e) {
             System.out.println(e.getMessage());
